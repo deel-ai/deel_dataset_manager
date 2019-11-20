d542e829b0b9d52fd631576cc38b87bcf579e58c
CAYSSIOLS Bertrand <bertrand.cayssiols@renault.com>
2019-11-20 16:07:32 +0100

    Update blink/tensorflow dataset
    Signed-off-by: CAYSSIOLS Bertrand <bertrand.cayssiols@renault.com>

--- deel/datasets/blink/blink_dataset.py
+++ deel/datasets/blink/blink_dataset.py
@@ -5,7 +5,7 @@ class DatasetBlink(Dataset):
     def __init__(self):
         super()
         self.urls = [
-            'https://datasets.deel.ai/blink/latest/blink-3.0-20191004.zip'
+            'https://datasets.deel.ai/blink/latest/blink-3.0.1-20191120.zip'
         ]
     
     def load(self, forceDownload=False):
