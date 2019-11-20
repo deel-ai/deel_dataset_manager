a73d5dfb85a17ece11726496621fba151bf31711
CAYSSIOLS Bertrand <bertrand.cayssiols@renault.com>
2019-11-20 16:07:32 +0100

    Update blink/tensorflow dataset
    Signed-off-by: CAYSSIOLS Bertrand <bertrand.cayssiols@renault.com>

--- examples/blink/tensorflow/example.py
+++ examples/blink/tensorflow/example.py
@@ -17,12 +17,12 @@ if __name__ == '__main__':
     _DEELDATASET_BASE_DIR = str(Path.home().joinpath('.deeldataset'))
 
     # Prediction on 1 image
-    img_path = path.join(_DEELDATASET_BASE_DIR, "blink", "blink-3.0-20191004.zip_extracted", "final_db_anonymous", "noblink", "nb_8.bmp")
+    img_path = path.join(_DEELDATASET_BASE_DIR, "blink", "blink-3.0.1-20191120.zip_extracted", "final_db_anonymous", "noblink", "nb_8.bmp")
     pred_index = aModel.predict(img_path)
     print(label_names[pred_index])
 
     # Prediction overall image inside blink_right folder
-    images = [f for f in glob.glob(path.join(_DEELDATASET_BASE_DIR, "blink", "blink-3.0-20191004.zip_extracted", "final_db_anonymous", "blink_right", "*.bmp"), recursive=True)]
+    images = [f for f in glob.glob(path.join(_DEELDATASET_BASE_DIR, "blink", "blink-3.0.1-20191120.zip_extracted", "final_db_anonymous", "blink_right", "*.bmp"), recursive=True)]
     for cur_img in images:
         pred_index = aModel.predict(cur_img)
         print(label_names[pred_index])
