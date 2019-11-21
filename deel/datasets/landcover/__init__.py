0e11606576e652d5bd4875220b4699c05afddc66
Mikaël Capelle <mikael.capelle@irt-saintexupery.com>
2019-11-21 14:27:02 +0100

    [landcover] Add datasets with bias on resolution.

--- deel/datasets/landcover/__init__.py
+++ deel/datasets/landcover/__init__.py
@@ -3,19 +3,25 @@
 import h5py
 import os
 
-from .landcover_dataset import LandCoverDataset
+from .landcover_dataset import LandCoverDataset, LandCoverResolutionDataset
 
-def load(framework=None, forceDownload=False):
+
+def load(bias='distribution', framework=None, forceDownload=False):
 
     # Load the dataset
-    dataset = LandCoverDataset()
+    if bias == 'distribution':
+        dataset = LandCoverDataset()
+    elif bias == 'resolution':
+        dataset = LandCoverResolutionDataset()
+    else:
+        raise ValueError('Unknown bias for landcover dataset.')
     dataset.load(forceDownload)
 
     # No framework, load the dataset from the HDF5 file as numpy arrays
     if framework is None:
-        path = os.path.join(dataset.unzippedPaths[0], 'landcover-1.0-20191015.h5')
+        path = os.path.join(
+            dataset.unzippedPaths[0],  dataset.h5_file)
         hdf5 = h5py.File(path, 'r')
-        patches = hdf5['patches'][:]
-        labels = hdf5['labels'][:]
+        data = tuple(hdf5[k][:] for k in dataset.h5_keys)
         hdf5.close()
-        return patches, labels
+        return data
--- deel/datasets/landcover/landcover_dataset.py
+++ deel/datasets/landcover/landcover_dataset.py
@@ -1,14 +1,33 @@
 # -*- encoding: utf-8 -*-
 
 from deel.datasets.base.dataset import Dataset
-import os
+
 
 class LandCoverDataset(Dataset):
+
+    h5_keys = ('patches', 'labels')
+    h5_file = 'landcover-1.0-20191015.h5'
+
     def __init__(self):
         super()
         self.urls = [
-            'https://datasets.deel.ai/landcover/latest/landcover-1.0-20191015.h5'
+            'https://datasets.deel.ai/landcover/latest/' + self.h5_file
         ]
-    
+
     def load(self, forceDownload=False):
         super().load('landcover', forceDownload)
+
+
+class LandCoverResolutionDataset(Dataset):
+
+    h5_keys = ('patches', 'labels', 'sensibilities')
+    h5_file = 'landcover-resolution-1.0-20191121.h5'
+
+    def __init__(self):
+        super()
+        self.urls = [
+            'https://datasets.deel.ai/landcover-resolution/latest/' + self.h5_file
+        ]
+
+    def load(self, forceDownload=False):
+        super().load('landcover-resolution', forceDownload)
