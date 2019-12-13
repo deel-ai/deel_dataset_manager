747747d37354bed5bfb603ba1a11791d4190fe5f
Mikaël Capelle <mikael.capelle@irt-saintexupery.com>
2019-12-13 14:26:48 +0100

    [settings] Add read_settings and write_settings method and update Settings constructor.

--- deel/datasets/settings.py
+++ deel/datasets/settings.py
@@ -1,10 +1,11 @@
 # -*- encoding: utf-8 -*-
 
+import collections
 import os
-import typing
 import yaml
 
 from pathlib import Path
+from typing import Any, Dict, TextIO
 
 from . import logger
 from .providers import make_provider, Provider
@@ -23,15 +24,9 @@ DEFAULT_FILE_LOCATION: Path = Path(
 DEFAULT_DATASETS_PATH = Path.home().joinpath(".deel", "datasets")
 
 
-class ParseSettingsError(Exception):
-    """ Exception raised if an issue occurs while parsing the settings. """
-
-    pass
-
-
 class Settings(object):
 
-    """ The `settings` class is a read-only class that contains
+    """ The `Settings` class is a read-only class that contains
     settings for the `deel.datasets` package.
 
     Settings are stored in a YAML format. The default location
@@ -40,87 +35,35 @@ class Settings(object):
     specify the default location of the file. """
 
     # Version of the settings:
-    _version: int = 0
+    _version: int
 
     # Type of the provider for the datasets (gcloud / manual / webdav):
-    _provider_type: str = "webdav"
+    _provider_type: str
 
     # Options for the provider:
-    _provider_options: typing.Dict[str, typing.Any] = {
-        # Authentication method (if any):
-        "auth": {
-            "method": "simple",
-            "username": "deel-datasets",
-            "password": "e]{qE/Pc65z'Nt?zLe-cK!_y?6f6",
-        },
-        # URL for the provider (if any):
-        "url": "https://datasets.deel.ai",
-    }
+    _provider_options: Dict[str, Any] = {}
 
     # The root folder containing the datasets:
-    _base: Path = DEFAULT_DATASETS_PATH
+    _base: Path
 
-    def __init__(self, io: typing.Optional[typing.TextIO] = None):
+    def __init__(
+        self,
+        version: int,
+        provider_type: str,
+        provider_options: Dict[str, Any],
+        path: Path,
+    ):
         """
         Args:
-            io: File-like object containing the configuration. If `None`,
-            created a default configuration.
-
-        Raises:
-            yaml.YAMLError: If the given stream does not contain valid YAML.
-            ParseSettingsError: If the given YAML is not valid for settings.
+            version: Version of the settings.
+            provider_type: Type of the provider.
+            provider_options: Options for the provider.
+            path: Local storage path for the datasets.
         """
-
-        if io is None:
-            return
-
-        # We let the error propagate to distinguish between error in
-        # parsing YAML and error in constructing settings:
-        data = yaml.safe_load(io)
-
-        # Retrieve the version:
-        if "version" not in data:
-            raise ParseSettingsError("Missing version.")
-        self._version = int(data["version"])
-
-        # Retrieve the provider:
-        if "provider" in data:
-            if isinstance(data["provider"], dict):
-                self._provider_type = data["provider"]["type"]
-                self._provider_options = data["provider"]
-                del self._provider_options["type"]
-            else:
-                self._provider_type = data["provider"]
-                self._provider_options = {}
-
-        # Retrieve the base folder:
-        if "path" in data:
-            self._base = Path(data["path"])
-        else:
-            self._base = self._get_default_path(self._provider_type)
-
-    def _get_default_path(self, provider_type: str) -> Path:
-        """ Retrieve the default dataset root path for the given provider type.
-
-        Args:
-            provider_type: The type of provider to retrieve the path for.
-
-        Returns:
-            The default path for the given provider.
-        """
-
-        # Default path is $HOME/.deel/datasets
-        path = DEFAULT_DATASETS_PATH
-
-        # For GCloud, we try to find the mount point:
-        if provider_type == "gcloud":
-            from ._gcloud_utils import find_gcloud_mount_path
-
-            gcloud_path = find_gcloud_mount_path()
-            if gcloud_path is not None:
-                path = gcloud_path
-
-        return path
+        self._version = version
+        self._provider_type = provider_type
+        self._provider_options = provider_options
+        self._base = path
 
     def make_provider(self) -> Provider:
         """ Creates and returns the provider corresponding to these settings.
@@ -145,6 +88,130 @@ class Settings(object):
         )
 
 
+def _get_default_path(provider_type: str) -> Path:
+    """ Retrieve the default dataset root path for the given provider type.
+
+    Args:
+        provider_type: The type of provider to retrieve the path for.
+
+    Returns:
+        The default path for the given provider.
+    """
+
+    # Default path is $HOME/.deel/datasets
+    path = DEFAULT_DATASETS_PATH
+
+    # For GCloud, we try to find the mount point:
+    if provider_type == "gcloud":
+        from ._gcloud_utils import find_gcloud_mount_path
+
+        gcloud_path = find_gcloud_mount_path()
+        if gcloud_path is not None:
+            path = gcloud_path
+
+    return path
+
+
+class ParseSettingsError(Exception):
+    """ Exception raised if an issue occurs while parsing the settings. """
+
+    pass
+
+
+def read_settings(stream: TextIO) -> Settings:
+    """ Load `Settings` from the given YAML stream.
+
+    Args:
+        stream: File-like object containing the configuration.
+
+    Returns:
+        A `Settings` object constructed from the given YAML stream.
+
+    Raises:
+        yaml.YAMLError: If the given stream does not contain valid YAML.
+        ParseSettingsError: If the given YAML is not valid for settings.
+    """
+
+    # We let the error propagate to distinguish between error in
+    # parsing YAML and error in constructing settings:
+    data = yaml.safe_load(stream)
+
+    # Retrieve the version:
+    if "version" not in data:
+        raise ParseSettingsError("Missing version.")
+    version = int(data["version"])
+
+    # Retrieve the provider:
+    if "provider" not in data:
+        raise ParseSettingsError("Missing provider.")
+
+    if isinstance(data["provider"], dict):
+        provider_type = data["provider"]["type"]
+        provider_options = data["provider"]
+        del provider_options["type"]
+    else:
+        provider_type = data["provider"]
+        provider_options = {}
+
+    # Retrieve the base folder:
+    if "path" in data:
+        path = Path(data["path"])
+    else:
+        path = _get_default_path(provider_type)
+
+    return Settings(version, provider_type, provider_options, path)
+
+
+def write_settings(settings: Settings, stream: TextIO, **kwargs):
+    """ Write the given `Settings` to the given stream as YAML.
+
+    Args:
+        settings: Settings to write.
+        stream: File-like object where the configuration will be written.
+        **kwargs: Extra arguments for the `yaml.safe_dump` method.
+    """
+
+    yaml.add_representer(
+        collections.OrderedDict,
+        lambda self, data: yaml.representer.SafeRepresenter.represent_dict(
+            self, data.items()
+        ),
+    )
+
+    # Build the python object:
+    data: collections.OrderedDict = collections.OrderedDict()
+
+    data["version"] = settings._version
+    data["provider"] = collections.OrderedDict()
+    data["provider"]["type"] = settings._provider_type
+    data["provider"].update(settings._provider_options)
+    data["path"] = str(settings._base.absolute())
+
+    yaml.dump(data, stream, **kwargs)
+
+
+# Default settings if the config.yml file is not found:
+_DEFAULT_SETTINGS = Settings(
+    # Version not set (0):
+    version=0,
+    # Provider WebDAV by default:
+    provider_type="webdav",
+    # With the DEEL remote server:
+    provider_options={
+        # Authentication method:
+        "auth": {
+            "method": "simple",
+            "username": "deel-datasets",
+            "password": "e]{qE/Pc65z'Nt?zLe-cK!_y?6f6",
+        },
+        # URL for the provider (if any):
+        "url": "https://datasets.deel.ai",
+    },
+    # The root folder containing the datasets:
+    path=DEFAULT_DATASETS_PATH,
+)
+
+
 def get_default_settings() -> Settings:
     """ Retrieve the default settings for the current machine.
 
@@ -152,12 +219,12 @@ def get_default_settings() -> Settings:
         The default settings for the current machine.
     """
 
-    settings: Settings = Settings()
+    settings: Settings = _DEFAULT_SETTINGS
 
     if DEFAULT_FILE_LOCATION.exists():
         try:
             with open(DEFAULT_FILE_LOCATION, "r") as fp:
-                settings = Settings(fp)
+                settings = read_settings(fp)
         except Exception:
             logger.warning(
                 "Failed to load deel.datasets settings from {}.".format(
