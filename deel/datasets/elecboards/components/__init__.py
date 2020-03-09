7d4c5ff2b373b6c53b1ea1869979c367c15f2aea
CAPELLE Mikaël <mikael.capelle@IRT-AESE.local>
2020-03-09 10:42:21 +0100

    [components] Add tensorflow mode with option to aggregate rotations.

--- deel/datasets/elecboards/components/__init__.py
+++ deel/datasets/elecboards/components/__init__.py
@@ -25,3 +25,52 @@ class ElecboardsComponentsDataset(Dataset):
 
     def load_path(self, path: pathlib.Path) -> pathlib.Path:
         return path.joinpath("components")
+
+    def load_tensorflow(
+        self,
+        path: pathlib.Path,
+        percent_train: float = 0.4,
+        percent_val: float = 0.4,
+        shuffle: typing.Union[int, bool] = True,
+        image_size: typing.Tuple[int, int] = (64, 64),
+        aggregate_rotation: bool = False,
+    ):
+        """ Load method for the `tensorflow` mode.
+
+        Args:
+            percent_train: Percentage of training data ([0, 1]).
+            percent_val: Percentage of validation data ([0, 1]).
+            shuffle: True to shuffle, or the random seed to use to shuffle data,
+            or False to not shuffle at all.
+            image_size: Size of the generated image.
+            aggregate_rotation: If True, rotations for a single components will
+            aggregated into a single class, if False, no aggregation is made and
+            each rotation of each component will be a different class.
+
+        Returns:
+            A tuple `(train, valildation, test)` where `train`, `validation`
+            and `test` are `tensorflow.data.Dataset`s corresponding
+            to training, validation data and test data.
+        """
+        from ...utils import load_tensorflow_image_dataset
+
+        if aggregate_rotation:
+            # Aggregate rotation classes:
+            def aggregate_fn(x: str) -> str:
+                split = x.split("_")
+                if split[-1].startswith("Rot"):
+                    split = split[:-1]
+                return "_".join(split)
+
+        else:
+            # Default aggregate function:
+            def aggregate_fn(x: str) -> str:
+                return x
+
+        return load_tensorflow_image_dataset(
+            self.load_path(path),
+            image_size=image_size,
+            train_split=(percent_train, percent_val),
+            shuffle=shuffle,
+            aggregate_fn=aggregate_fn,
+        )
