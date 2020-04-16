f6550a1079aee046817b4ad0e8ecb37617c7407a
uidn4228 <thomas.soumarmon@continental-corporation.com>
2020-04-16 20:12:07 +0200

    [blink] Add PyTorch loader iso-functional to tensorflow for blink dataset.

--- deel/datasets/utils.py
+++ deel/datasets/utils.py
@@ -2,8 +2,11 @@
 
 import pathlib
 import random
+import logging
 
 from typing import List, Tuple, Optional, Union, Dict, Callable
+
+log = logging.getLogger("deel.dataset.manager.utils")
 
 
 def load_python_image_dataset(
@@ -156,6 +159,206 @@ def load_numpy_image_dataset(
             (images[train_n:val_n], labels[train_n:val_n]),
             (images[val_n:], labels[val_n:]),
         )
+
+
+def aggregate_pytorch_dataset(
+    dataset, aggregate_fn: Callable[[str], Optional[str]] = None
+):
+    """ Aggregate classes in a pytorch image dataset from.
+
+    Args:
+        dataset: The dataset.
+        aggregate_fn: Callable to aggregate classes. The function should take
+        the name of an original class (subfolder) and returns the name of the
+        "parent" class. If the call returns `None`, the class is discarded.
+
+    Returns: The aggregated dataset.
+    """
+    if aggregate_fn:  # there is an aggregation filter function, so do aggregate
+        # run aggregation function on classes
+        new_classes = {cls: aggregate_fn(cls) for cls in dataset.classes}
+        new_classes_list = list(set(new_classes.values()))
+        if len(new_classes_list) < len(dataset.classes):
+            log.debug(
+                "aggregation changed classes count {} => {}".format(
+                    len(dataset.classes), len(new_classes_list)
+                )
+            )
+            # number of class is different, modify indices to reflect changes
+            convert_old_class_new = {
+                old_idx: new_classes_list.index(new_classes[dataset.classes[old_idx]])
+                for old_idx in range(len(dataset.classes))
+            }
+            dataset.samples = dataset.imgs = [
+                (img[0], convert_old_class_new[img[1]]) for img in dataset.imgs
+            ]
+            dataset.targets = [convert_old_class_new[cls] for cls in dataset.targets]
+
+        # only names may have changed
+        dataset.classes = new_classes_list
+        dataset.class_to_idx = {cls: idx for idx, cls in enumerate(new_classes_list)}
+
+    return dataset
+
+
+def filter_pytorch_dataset(
+    dataset, filter_fn: Callable[[str, pathlib.Path], bool] = None
+):
+    """ Filters a pytorch image dataset from.
+
+    Args:
+        dataset: The dataset.
+        filter_fn: A function to filter out images. This function should take
+        a string (name of the file) and a path to the file and returns True
+        if the image should be included, False if it should be excluded.
+
+    Returns: The filtered dataset.
+    """
+    if filter_fn:  # there is a filter function, so do filter
+        modified = False
+        imgs, targets = [], []
+
+        # inverse dict dataset.class_to_idx
+        idx_to_class_str = {v: k for k, v in dataset.class_to_idx.items()}
+        log.debug("filtering {} elements".format(len(dataset)))
+        for cls, img in zip(dataset.targets, dataset.imgs):
+            if filter_fn(idx_to_class_str[cls], img[0]):
+                targets.append(cls)
+                imgs.append(img)
+            else:
+                modified = True
+        if modified:
+            all_classes_idx_new = list(set(targets))
+            if len(all_classes_idx_new) < len(dataset.classes):
+                log.debug(
+                    "filtering changed classes count {} => {}".format(
+                        len(dataset.classes), len(all_classes_idx_new)
+                    )
+                )
+                # some classes were filtered out so we remove them and recompute indices
+                convert_old_class_new = {
+                    val: idx for idx, val in enumerate(all_classes_idx_new)
+                }
+                new_classes = [dataset.classes[cls] for cls in all_classes_idx_new]
+                targets = [convert_old_class_new[cls] for cls in targets]
+                imgs = [(img[0], convert_old_class_new[img[1]]) for img in imgs]
+                dataset.classes = new_classes
+                dataset.class_to_idx = {cls: idx for idx, cls in enumerate(new_classes)}
+
+            dataset.samples = dataset.imgs = imgs
+            dataset.targets = targets
+            log.debug("filtered {} elements".format(len(dataset)))
+        else:
+            log.debug("No element filtered out")
+
+        # log.debug("classes idx", set([el[1] for el in dataset.samples]))
+        # log.debug("classes idx", set([el[1] for el in dataset.imgs]))
+    return dataset
+
+
+def split_pytorch_dataset(
+    dataset,
+    train_split: Union[float, Tuple[float, float]] = 0.8,
+    shuffle: Union[bool, int] = True,
+):
+    """ Splits the pytorch image dataset into train, [valid,] test.
+
+    Args:
+        dataset: The dataset to split.
+        train_split: One or two float values. If a single value is specified,
+        two datasets will be returned, one for training (using a percentage
+        `train_split` of data) and one for testing. If two values are specified,
+        three datasets will be returned: a training dataset, a validation
+        dataset and a testing dataset.
+        shuffle: If True, shuffle images before spliting with the default seed.
+        If an int is given, use it as the seed for shufling. If False, do not
+        shuffle.
+        False otherwize.
+
+    Returns: Two or three datasets corresponding to training, validation and
+    testing dataset.
+    """
+    import numpy as np
+    from torch.utils.data import Subset
+
+    # list the indices of the dataset
+    dataset_size = len(dataset)
+    indices = list(range(dataset_size))
+    if shuffle:
+        np.random.shuffle(indices)
+
+    # Thank you tox for not accepting a simplier expression...
+    if isinstance(train_split, float):
+        train_splits = [
+            train_split,
+        ]
+    else:
+        train_splits = list(train_split)
+
+    numbers = [int(np.floor(percent * dataset_size)) for percent in train_splits]
+    splits = [0]
+    split = 0
+    for number in numbers:
+        split += number
+        splits.append(split)
+    if splits[-1] < dataset_size:
+        splits.append(dataset_size)
+
+    all_indices = [
+        indices[split_min:split_max]
+        for split_min, split_max in zip(splits[:-1], splits[1:])
+    ]
+
+    return tuple(Subset(dataset, split_indices) for split_indices in all_indices)
+
+
+def load_pytorch_image_dataset(
+    folder: pathlib.Path,
+    image_size: Optional[Tuple[int, int]] = None,
+    train_split: Union[float, Tuple[float, float]] = 0.8,
+    shuffle: Union[bool, int] = True,
+    aggregate_fn: Callable[[str], Optional[str]] = None,
+    filter_fn: Callable[[str, pathlib.Path], bool] = None,
+    transform: Callable = None,
+):
+    """ Creates a pytorch image dataset from the given folder and
+    parameters.
+
+    Args:
+        folder: The folder containing the dataset. The folder should contain,
+        for each classes, a subfolder with only images inside.
+        image_size: The size of the image, or None to not resize images.
+        train_split: One or two float values. If a single value is specified,
+        two datasets will be returned, one for training (using a percentage
+        `train_split` of data) and one for testing. If two values are specified,
+        three datasets will be returned: a training dataset, a validation
+        dataset and a testing dataset.
+        shuffle: If True, shuffle images before spliting with the default seed.
+        If an int is given, use it as the seed for shufling. If False, do not
+        shuffle.
+        False otherwize.
+        aggregate_fn: Callable to aggregate classes. The function should take
+        the name of an original class (subfolder) and returns the name of the
+        "parent" class. If the call returns `None`, the class is discarded.
+        filter_fn: A function to filter out images. This function should take
+        a string (name of the file) and a path to the file and returns True
+        if the image should be included, False if it should be excluded.
+
+    Returns: Two or three datasets corresponding to training, validation and
+    testing dataset.
+    """
+    from torchvision.datasets import ImageFolder
+
+    # create PyTorch dataset from image folder
+    dataset = ImageFolder(folder, transform=transform)
+    # use filter
+    if filter_fn:
+        dataset = filter_pytorch_dataset(dataset, filter_fn=filter_fn)
+    if aggregate_fn:
+        dataset = aggregate_pytorch_dataset(dataset, aggregate_fn=aggregate_fn)
+
+    # split the dataset
+    return split_pytorch_dataset(dataset, train_split=train_split, shuffle=shuffle)
 
 
 def load_tensorflow_image_dataset(
