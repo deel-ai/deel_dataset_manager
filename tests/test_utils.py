62949e9da0dd006837b1e277aabbe9683f590da0
Justin Plakoo <justin.plakoo@irt-saintexupery.com>
2020-11-02 16:50:35 +0100

    Update utils tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,7 +7,9 @@ import typing
 from deel.datasets.utils import (
     load_pytorch_image_dataset,
     load_numpy_image_dataset,
+    load_tensorflow_image_dataset,
     load_hierarchical_python_image_dataset,
+    split_on_label,
 )
 
 path_hierachical = pathlib.Path(
@@ -23,6 +25,17 @@ path = pathlib.Path(
         "data/dataset3/1.0.0/bottle/ground_truth",
     )
 )
+
+n_images_file = 0
+for dir in path.iterdir():
+    for f in path.joinpath(dir).glob("*.png"):
+        n_images_file += 1
+
+n_train_img = int(n_images_file * 0.8)
+
+n_broken_small_img = 0
+for _ in path.joinpath("broken_small").glob("*.png"):
+    n_broken_small_img += 1
 
 
 def dispatch_fn(
@@ -74,9 +87,7 @@ def test_load_pytorch_image_dataset():
         train_split=0.8,
     )
 
-    # print(len(dataset))
-    assert len(dataset) == 2
-    # print(idx_to_class)
+    assert len(dataset[0]) == n_train_img
     assert idx_to_class == {0: "broken_large", 1: "broken_small", 2: "contamination"}
 
 
@@ -90,8 +101,52 @@ def test_load_numpy_image_dataset():
         image_size=(64, 64),
         train_split=0.8,
     )
-
-    # print(len(dataset))
-    assert len(dataset) == 2
-    # print(idx_to_class)
+    assert len(dataset[0][0]) == n_train_img
     assert idx_to_class == {0: "broken_large", 1: "broken_small", 2: "contamination"}
+
+
+def test_numpy_split_on_label():
+    """
+    Test the numpy split on label methos
+    """
+    dataset, _ = load_numpy_image_dataset(
+        path,
+        image_size=(64, 64),
+        train_split=0.8,
+    )
+    (train_in, train_out), (test_in, test_out) = split_on_label(dataset, [1])
+    assert len(train_in[0]) + len(test_in[0]) == n_broken_small_img
+    assert len(train_in[0]) + len(train_out[0]) == len(dataset[0][0])
+    assert len(test_in[0]) + len(test_out[0]) == len(dataset[1][0])
+
+
+def test_torch_split_on_label():
+    """
+    Test the torch split on label methos
+    """
+
+    dataset, _ = load_pytorch_image_dataset(
+        path,
+        image_size=(64, 64),
+        train_split=0.8,
+    )
+    (train_in, train_out), (test_in, test_out) = split_on_label(dataset, [1])
+    assert len(train_in) + len(test_in) == n_broken_small_img
+    assert len(train_in) + len(train_out) == len(dataset[0])
+    assert len(test_in) + len(test_out) == len(dataset[1])
+
+
+def test_tensorflow_split_on_label():
+    """
+    Test the tensorflow split on label methos
+    """
+
+    dataset, _ = load_tensorflow_image_dataset(
+        path,
+        image_size=(64, 64),
+        train_split=0.8,
+    )
+    (train_in, train_out), (test_in, test_out) = split_on_label(dataset, [1])
+    assert len(train_in) + len(test_in) == n_broken_small_img
+    assert len(train_in) + len(train_out) == len(dataset[0])
+    assert len(test_in) + len(test_out) == len(dataset[1])
