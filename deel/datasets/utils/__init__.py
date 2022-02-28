9abcc60e6672ffe7a113b7b7fd6fa5b282f46ea4
Justin Plakoo <justin.plakoo@irt-saintexupery.com>
2022-02-28 13:55:54 +0000

    Update __init__.py

--- deel/datasets/utils/__init__.py
+++ deel/datasets/utils/__init__.py
@@ -20,6 +20,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+# flake8: noqa
 from .supervised import (
     load_hierarchical_python_image_dataset,
     load_hierarchical_pytorch_image_dataset,
